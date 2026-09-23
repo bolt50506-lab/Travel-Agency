6d89aa9df4329f9582e6d385bab14506ba0d8e10
bolt50506-lab <bolt50506@gmail.com>
2026-09-23 11:47:43 +0500

    Make PostgREST role password safely environment-driven

--- self-hosted/00-roles.sh
+++ self-hosted/00-roles.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOSQL
-DO \$\$
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --set=postgrest_password="$POSTGREST_DB_PASSWORD" <<'EOSQL'
+DO $$
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'authenticator') THEN
-    CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD '${POSTGREST_DB_PASSWORD}';
-  ELSE
-    ALTER ROLE authenticator PASSWORD '${POSTGREST_DB_PASSWORD}';
+    CREATE ROLE authenticator NOINHERIT LOGIN;
   END IF;
   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'anon') THEN
     CREATE ROLE anon NOLOGIN;
@@ -15,5 +13,7 @@ BEGIN
     CREATE ROLE service_role NOLOGIN;
   END IF;
 END
-\$\$;
+$$;
+
+ALTER ROLE authenticator PASSWORD :'postgrest_password';
 EOSQL
