3f889585673a32ea4f392ecfd878d50f45243e0b
bolt50506-lab <bolt50506@gmail.com>
2026-09-23 15:59:30 +0500

    Add blog public information page

---
+++ app/(customer)/blog/page.tsx
@@ -0,0 +1,5 @@
+import { InfoPage } from '@/components/shared/info-page';
+
+export default function Page() {
+  return <InfoPage title="Travel insights" intro="Practical travel information from Destino Travels." sections={[{"heading":"Travel guidance","body":"Destination advice, booking guidance and agency updates can be published here as the content library grows."}]} />;
+}
